45a6c77ebc176570b05690f4545ec58b49e05c02
aaron <alkrauss48@gmail.com>
2025-07-22 10:14:33 -0500

    chore: update zombie recipe

--- src/lib/data/cocktails/zombie.ts
+++ src/lib/data/cocktails/zombie.ts
@@ -5,16 +5,17 @@ const ZOMBIE: Cocktail = {
 	title: 'Zombie',
 	subtitle: 'The original heavy-weight tiki classic. Max 2 per person.',
 	description:
-		"Blended light rum, Jamaican rum, overproof demerara rum, falernum, Donn's Mix, lime, grenadine, herbstura.",
+		"Blended light rum, Jamaican rum, overproof rum, falernum, Donn's Mix, lime, grenadine, herbstura.",
 	imagePath: '/images/cocktails/tiki/zombie.png',
 	slug: 'zombie',
 	method: CocktailMethod.FlashBlended,
 	ingredients: [
+		'1oz Appleton Estate Signature (Jamaican rum)',
 		'.5oz Probitas (Blended light rum)',
 		'.5oz Coruba (Jamaican rum)',
-		'.1oz Appleton Estate Signature (Jamaican rum)',
-		'.1oz Hamilton 151 (Overproof Demerara rum)', // 151 is a 151 proof rum, which is 75.5% alcohol by volume.
-		".5oz Donn's Mix (1/2 Grapefruit, 1/2 Cinnamon Syrup)",
+		'.5oz Hamilton 151 (Overproof Demerara rum)',
+		'.5oz Planteray OFTD (Overproof Blended rum)',
+		".5oz Donn's Mix (2:1 ratio of Grapefruit to Cinnamon Syrup)",
 		'.5oz Falernum',
 		'.75oz Lime',
 		'1tsp Grenadine',
