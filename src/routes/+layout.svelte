c321cc48f45b2f88d6321d755ab15d91bbe53c36
aaron <alkrauss48@gmail.com>
2025-07-28 22:01:41 -0500

    fix: update og url setting to be dynamic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<meta property="og:url" content="https://thekrausshaus.com{$page.url.pathname}" />
+</svelte:head>
 
 {@render children()}
