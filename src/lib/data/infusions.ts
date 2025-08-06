a3922f06999723129dfabc4476ef4e9f4b187747
aaron <alkrauss48@gmail.com>
2025-08-06 10:27:15 -0500

    chore: move falernum from syrups to infusions

--- src/lib/data/infusions.ts
+++ src/lib/data/infusions.ts
@@ -24,5 +24,23 @@ export const infusions: Recipe[] = [
 		instructions:
 			'Add ingredients to a mason jar and let infuse for 2-3 hours, swirling around every 15-30 minutes. Remove any lingering pieces.',
 		notes: 'Used in the Peppermint Espresso Martini. This infusion will turn a nice pink/red color.'
-	}
+	},
+	{
+		name: 'Falernum',
+		description: 'A spiced liqueur with notes of ginger, clove, and lime',
+		ingredients: [
+			'8 oz overproof rum (like Planteray OFTD)',
+			'12 limes zested (not juiced)',
+			'24 cloves',
+			'30g ginger, chopped',
+			'1 tsp grated nutmeg',
+			'1 vanilla bean, split and scraped. Include seeds and pot',
+			'16 oz semi-rich (1.5:1) simple syrup made with oatmilk',
+			'1 oz amaretto'
+		],
+		instructions:
+			'Steep all ingredients in rum for 24 hours, besides simple syrup and amaretto. Strain to remove solids. Add simple syrup and amaretto.',
+		notes:
+			"Makes ~25oz at 23% ABV. Used in the Three Dots & a Dash, the Jet Pilot, the Cobra's Fang, the Saturn, and the Iron Ranger. Because of its alcohol content, this will keep for up to 2 years refrigerated. Recipe makes about 2.75-3 cups."
+	},
 ];
--- src/lib/data/syrups.ts
+++ src/lib/data/syrups.ts
@@ -62,24 +62,6 @@ export const syrups: Recipe[] = [
 		notes: 'Used in the Penicillin.'
 	},
 	{
-		name: 'Falernum',
-		description: 'A spiced syrup with notes of ginger, clove, and lime',
-		ingredients: [
-			'8 oz overproof rum (like Planteray OFTD)',
-			'12 limes zested (not juiced)',
-			'24 cloves',
-			'30g ginger, chopped',
-			'1 tsp grated nutmeg',
-			'1 vanilla bean, split and scraped. Include seeds and pot',
-			'16 oz semi-rich (1.5:1) simple syrup made with oatmilk',
-			'1 oz amaretto'
-		],
-		instructions:
-			'Steep all ingredients in rum for 24 hours, besides simple syrup and amaretto. Strain to remove solids. Add simple syrup and amaretto.',
-		notes:
-			"Used in the Three Dots & a Dash, the Jet Pilot, the Cobra's Fang, the Saturn, and the Iron Ranger. Because of its alcohol content, this will keep for up to 2 years refrigerated. Recipe makes about 2.75-3 cups."
-	},
-	{
 		name: 'Fassionola',
 		description: 'A tropical fruit syrup with a complex blend of flavors',
 		ingredients: [
