a75a62cb1d4784f9a76764b820e380fbbf67a3c8
aaron <alkrauss48@gmail.com>
2025-11-30 22:51:40 -0600

    feat: add forest-spirit cocktail

---
+++ src/lib/data/cocktails/forest-spirit.ts
@@ -0,0 +1,73 @@
+import { CocktailMethod } from '$lib/enums/methods';
+import { ServedIn } from '$lib/enums/served-in';
+import type { Cocktail } from '$lib/types/cocktails';
+import { Ingredients } from '../all-ingredients';
+import { Tags } from '../all-tags';
+
+const FOREST_SPIRIT: Cocktail = {
+	title: 'Forest Spirit',
+	subtitle: 'A Krauss Family original',
+	description:
+		'Gin, elderflower liqueur, italicus, lemon, honey syrup, orange bitters, soda water, mint.',
+	imagePath:
+		'https://personal-k8s-main-space.nyc3.cdn.digitaloceanspaces.com/thekrausshaus.com/images/cocktails/full/forest-spirit.png',
+	thumbnailImagePath:
+		'https://personal-k8s-main-space.nyc3.cdn.digitaloceanspaces.com/thekrausshaus.com/images/cocktails/thumbnail/forest-spirit.png',
+	slug: 'forest-spirit',
+	method: CocktailMethod.Shaken,
+	servedIn: ServedIn.DoubleRocksGlass,
+	ingredients: [
+		{
+			amount: '1.5oz',
+			ingredient: Ingredients.BaseSpirits.FORDS
+		},
+		{
+			amount: '.5oz',
+			ingredient: Ingredients.Liqueurs.ELDERFLOWER_LIQUEUR
+		},
+		{
+			amount: '.25oz',
+			ingredient: Ingredients.Liqueurs.ITALICUS
+		},
+		{
+			amount: '.5oz',
+			ingredient: Ingredients.Citrus.LEMON
+		},
+		{
+			amount: '.25oz',
+			ingredient: Ingredients.Syrups.HONEY_SYRUP
+		},
+		{
+			amount: '1 dash',
+			ingredient: Ingredients.Bitters.ORANGE
+		},
+		{
+			amount: '1-2 oz',
+			ingredient: Ingredients.Mixers.SODA_WATER
+		},
+		{
+			label: 'Garnish: Mint sprig',
+			ingredient: Ingredients.HerbsAndSpices.MINT
+		},
+		{
+			label: 'Garnish: Expressed lemon twist, discarded',
+			ingredient: Ingredients.Citrus.LEMON_GARNISH
+		},
+		'Garnish: Cocktail umbrella'
+	],
+	notes:
+		'Created as a themed drink for My Neighbor Totoro for our 7th anniversary. Shake all ingredients with ice except the soda water. Strain into a double rocks glass over ice. Top with soda water. Express and discard lemon twist. Garnish with a mint sprig and cocktail umbrella.',
+	tags: [
+		Tags.BaseAlcohol.GIN,
+		Tags.FlavorProfile.CITRUS,
+		Tags.FlavorProfile.HERBAL,
+		Tags.FlavorProfile.BUBBLY,
+		Tags.AlcoholLevel.LOW,
+		Tags.Technique.SHAKEN,
+		Tags.Style.SOUR,
+		Tags.Origin.ORIGINAL,
+		Tags.ServedIn.DOUBLE_ROCKS_GLASS
+	]
+};
+
+export default FOREST_SPIRIT;
