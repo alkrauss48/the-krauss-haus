89dab77f4b5d388443778ed6a366b0648ad52a6b
aaron <alkrauss48@gmail.com>
2025-12-07 22:31:49 -0600

    feat: add citron-vodka recipe and ingredient

--- src/lib/data/all-recipes.ts
+++ src/lib/data/all-recipes.ts
@@ -16,6 +16,7 @@ import TOM_AND_JERRY_BATTER from '$lib/data/recipes/tom-and-jerry-batter';
 
 // Import all infusion recipes
 import CARAMEL_VODKA from '$lib/data/recipes/caramel-vodka';
+import CITRON_VODKA from '$lib/data/recipes/citron-vodka';
 import FALERNUM from '$lib/data/recipes/falernum';
 import JALAPENO_TEQUILA from '$lib/data/recipes/jalapeno-tequila';
 import LIMONCELLO from '$lib/data/recipes/limoncello';
@@ -42,6 +43,7 @@ export const syrups: Recipe[] = [
 // Infusions
 export const infusions: Recipe[] = [
 	CARAMEL_VODKA,
+	CITRON_VODKA,
 	FALERNUM,
 	JALAPENO_TEQUILA,
 	LIMONCELLO,
---
+++ src/lib/data/recipes/citron-vodka.ts
@@ -0,0 +1,12 @@
+import type { Recipe } from '$lib/types/recipes';
+
+const CITRON_VODKA: Recipe = {
+	name: 'Citron Vodka',
+	slug: 'citron-vodka',
+	description: 'Vodka infused with lemon peel.',
+	ingredients: ['8 oz vodka', 'Peels of 1 lemon'],
+	instructions:
+		'Add ingredients to a mason jar and let infuse for 48-72 hours, swirling a few times a day. Strain.'
+};
+
+export default CITRON_VODKA;
