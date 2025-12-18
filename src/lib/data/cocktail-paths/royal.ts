d2e46c40b7da633b0e335eb37139c2b50d823047
aaron <alkrauss48@gmail.com>
2025-12-17 21:56:03 -0600

    feat: add innkeeper and royal cocktail paths

---
+++ src/lib/data/cocktail-paths/royal.ts
@@ -0,0 +1,17 @@
+import type { CocktailPath } from '$lib/types/cocktail-path';
+import BRANDY_ALEXANDER from '../cocktails/brandy-alexander';
+import CHOCOLATE_COVERED_CHERRIES from '../cocktails/chocolate-covered-cherries';
+import JACK_ROSE from '../cocktails/jack-rose';
+import PAINKILLER from '../cocktails/painkiller';
+import RAMOS_GIN_FIZZ from '../cocktails/ramos-gin-fizz';
+
+export const ROYAL: CocktailPath = {
+	title: 'Royal',
+	subtitle: 'Decadent. Refined. Indulgent.',
+	slug: 'royal',
+	imagePath:
+		'https://personal-k8s-main-space.nyc3.cdn.digitaloceanspaces.com/thekrausshaus.com/images/paths/royal.jpeg',
+	description:
+		'For those who savor luxury in every sip. The Royal path celebrates richness, ceremony, and indulgence—creamy textures, elegant spirits, and cocktails worthy of velvet curtains and crystal glassware.These are drinks made to be lingered over, admired, and enjoyed without restraint.',
+	cocktails: [PAINKILLER, JACK_ROSE, BRANDY_ALEXANDER, CHOCOLATE_COVERED_CHERRIES, RAMOS_GIN_FIZZ]
+};
