34f4fea171f61cf375276f691a329ed8b4fc4b7b
aaron <alkrauss48@gmail.com>
2025-12-18 20:46:07 -0600

    refactor: extract cocktail slug menu link logic to data component

--- src/routes/cocktails/[slug]/+page.svelte
+++ src/routes/cocktails/[slug]/+page.svelte
@@ -9,44 +9,9 @@
 	import type { Tag } from '$lib/types/tags';
 	import { getIngredientDisplayName, formatVariantIngredients } from '$lib/utils/ingredients';
 
-	// Import menu data to check which menus contain this cocktail
-	import {
-		categories as summerCategories,
-		featuredDrink as summerFeatured
-	} from '$lib/data/summer-menu';
-	import {
-		categories as winterCategories,
-		featuredDrink as winterFeatured
-	} from '$lib/data/winter-menu';
-	import { categories as tikiCategories } from '$lib/data/tiki-menu';
-
 	export let data: PageData;
-	const { cocktail, pathsContainingCocktail } = data;
+	const { cocktail, onSummer, onWinter, onTiki, pathsContainingCocktail } = data;
 
-	// Check which menus contain this cocktail
-	function isOnSummerMenu(): boolean {
-		if (summerFeatured.slug === cocktail.slug) return true;
-		return summerCategories.some((category) =>
-			category.cocktails.some((c) => c.slug === cocktail.slug)
-		);
-	}
-
-	function isOnWinterMenu(): boolean {
-		if (winterFeatured.slug === cocktail.slug) return true;
-		return winterCategories.some((category) =>
-			category.cocktails.some((c) => c.slug === cocktail.slug)
-		);
-	}
-
-	function isOnTikiMenu(): boolean {
-		return tikiCategories.some((category) =>
-			category.cocktails.some((c) => c.slug === cocktail.slug)
-		);
-	}
-
-	const onSummer = isOnSummerMenu();
-	const onWinter = isOnWinterMenu();
-	const onTiki = isOnTikiMenu();
 	const onAnyMenu = onSummer || onWinter || onTiki;
 	const hasMenusOrPaths = onAnyMenu || pathsContainingCocktail.length > 0;
 
