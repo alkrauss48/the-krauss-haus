2742781e18c6bab6746cdbb1e42911b4883cb0d4
aaron <alkrauss48@gmail.com>
2026-02-02 16:37:11 -0600

    refactor(tiki-menu): add cocktails to tiki menu

--- src/lib/data/tiki-menu.ts
+++ src/lib/data/tiki-menu.ts
@@ -14,6 +14,10 @@ import SATURN from '$lib/data/cocktails/saturn';
 import SINGAPORE_SLING from '$lib/data/cocktails/singapore-sling';
 import THREE_DOTS_AND_A_DASH from '$lib/data/cocktails/three-dots-and-a-dash';
 import ZOMBIE from '$lib/data/cocktails/zombie';
+import LOST_LAKE from '$lib/data/cocktails/lost-lake';
+import RUM_BARREL from '$lib/data/cocktails/rum-barrel';
+import DONGA_PUNCH from '$lib/data/cocktails/donga-punch';
+import PLANTERS_PUNCH from '$lib/data/cocktails/planters-punch';
 
 export const categories: Category[] = [
 	{
@@ -24,7 +28,7 @@ export const categories: Category[] = [
 			tertiary: '#ecfdf5',
 			variationText: '#166534'
 		},
-		cocktails: [PAINKILLER, JUNGLE_BIRD, ANCIENT_MARINER, HURRICANE]
+		cocktails: [PAINKILLER, JUNGLE_BIRD, ANCIENT_MARINER, DONGA_PUNCH, HURRICANE, PLANTERS_PUNCH]
 	},
 	{
 		title: 'Bold and Boozy',
@@ -34,7 +38,7 @@ export const categories: Category[] = [
 			tertiary: '#fdf2f8',
 			variationText: '#581c87'
 		},
-		cocktails: [MAI_TAI, THREE_DOTS_AND_A_DASH, COBRAS_FANG, ZOMBIE]
+		cocktails: [LOST_LAKE, MAI_TAI, THREE_DOTS_AND_A_DASH, RUM_BARREL, COBRAS_FANG, ZOMBIE]
 	},
 	{
 		title: 'Not Just Rum',
